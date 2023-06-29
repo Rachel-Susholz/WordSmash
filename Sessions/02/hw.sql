9d73602077cf14f4e1dc1927ee884e6373513061
shmielcpu <sminz@cpucodeschool.com>
2023-06-29 15:03:30 +0000

    #2 HW comments

--- Sessions/02/hw.sql
+++ Sessions/02/hw.sql
@@ -1,5 +1,6 @@
-
+-- SM Excellent! 80% See comments, fix and resubmit.
 --1 show me all medals won in the summer season by Greek or Spanish medalists, sorted by sport and then year
+-- SM Formatting tip: Each clause should be on new line
 select * from Medalist m 
 where m.Season = 'summer' 
 and m.Country in ('Greece', 'Spain') 
@@ -8,6 +9,7 @@ order by m.Sport, m.OlympicYear
 The Swiss government has requested a list of medalists from their country since 1928 who were over the age of 24 when they won their medal.
 Include only First name, last name, age when they won the medal sorted by sport and sport category.
 */
+-- SM Formatting tip: Each clause should be on new line
 select m.FirstName, m.LastName, AgeWhenWonMedal = m.OlympicYear - m.YearBorn from Medalist m 
 where m.Country = 'Switzerland'
 and m.OlympicYear > 1928
@@ -17,6 +19,8 @@ order by m.Sport, m.SportSubcategory
 For all medalists that won a gold medal for a some sort of skating show their first name, last name, olympic location, sport and year.
 Sort by year and sport.
 */
+-- SM Formatting tip: Each clause should be on new line
+-- You have a typo in your like statement so it doesn't return anything
 select m.FirstName, m.LastName, m.OlympicLocation, m.Sport, m.OlympicYear from Medalist m 
 where m.Medal = 'gold'
 and m.Sport like '%skating&'
@@ -27,6 +31,7 @@ A clue of foul play was found for silver and gold medalists under 30 years old w
 Provide the a list of medalists that match that clue, exclude the MedalistId, Code, and Year Born, include the age
 Sort by age high to low, medal, last name
 */
+-- SM Formatting tip: Each clause should be on new line
 select AgeWhenWonMedal = m.OlympicYear - m.YearBorn, m.Country, m.FirstName, m.LastName, m.Medal, m.OlympicLocation, m.OlympicYear, m.Season, m.Sport, m.SportSubcategory from medalist m 
 where m.Medal in ('silver','gold')
 and m.OlympicYear - m.YearBorn < 30
@@ -38,5 +43,7 @@ Can you produce a list of inventions that mention war in their description.
 Be careful not to include descriptions that have words like hardware or forward, but do include words that begin with war like warhead. 
 We may get some that are not related, like if has "wart", that is fine, we will remove those manually (no pun intended)
 */
+-- SM Formatting tip: Each clause should be on new line
+-- This doesn't return anything.
 select * from invention i 
 where i.InventionDesc like 'war%'
