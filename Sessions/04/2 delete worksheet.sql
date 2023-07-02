710d8a26c9bab537fc1cb0de9f7d831a258d6293
Rachel-Susholz <rochelsusholz@gmail.com>
2023-07-02 16:16:05 -0400

    #4

--- Sessions/04/2 delete worksheet.sql
+++ Sessions/04/2 delete worksheet.sql
@@ -1,8 +1,15 @@
 --Invention, re-insert rows after each delete
 --1. Delete all rows using the shorthand syntax
-
+delete Invention
 --2. Delete all rows in the table, using the longhand syntax
-
+delete i from Invention i
 --3. Delete all where the description contains the word "war", either separate or as a compound word like "warfare". First do as select, then convert to delete
-
+delete i
+--select *
+from Invention i 
+where i.InventionDesc like '% war%'
 --4 Delete all where the inventor has two first names, do not delete records with multiple inventors
+select *
+from Invention i 
+where i.InventorFirstName like '% %'
+and i.InventorFirstName not like '% & %'
--- Sessions/04/HW.sql
+++ Sessions/04/HW.sql
@@ -1,15 +1,19 @@
 --For all questions do long-hand delete unless otherwise specified, first do as select and then delete, after each question re-insert the data
 
 --1 Delete all medalist records using shorthand syntax
-
+delete Medalist
 --2 Delete all medalist records using longhand syntax
-
+delete m from Medalist m
 --3 In medalist delete all where age of medalist is less than 18 or over 60, in your select statement show the age
+delete m 
+--select AgeOfMedalist = m.OlympicYear - m.YearBorn
+from medalist m
+where (m.OlympicYear - m.YearBorn < 18 or m.OlympicYear - m.YearBorn > 60)
 
 --4 In medalist delete all medalists with two last names, with or without a hyphen (Smith Jones or Smith-Jones). Only show the first and last name in your select statement. 
-
-/*5 
-In invention delete all records where it is impossible that this inventor created this invention (based on year born or died). 
-In select statement show columns in this order Invention Name, Inventor Last Name, Year Invented, Year Born, Year Died, Age at time of invention
-*/
+delete m
+--select m.FirstName, m.LastName
+from medalist m 
+where (m.LastName like '% %' or m.LastName like '%-%')
+and m.LastName not like '% % %'
 
