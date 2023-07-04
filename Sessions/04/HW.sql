89120200f53940bfc14184f3705e532e957ce464
shmielcpu <sminz@cpucodeschool.com>
2023-07-04 13:20:05 +0000

    #4 HW mark

--- Sessions/04/HW.sql
+++ Sessions/04/HW.sql
@@ -1,4 +1,4 @@
--- SM Excellent! You're missing one question. See comments, fix and resubmit.
+-- SM Excellent! 100% See comments, no need to resubmit.
 --For all questions do long-hand delete unless otherwise specified, first do as select and then delete, after each question re-insert the data
 
 --1 Delete all medalist records using shorthand syntax
@@ -6,7 +6,6 @@ delete Medalist
 --2 Delete all medalist records using longhand syntax
 delete m from Medalist m
 --3 In medalist delete all where age of medalist is less than 18 or over 60, in your select statement show the age
--- SM Tip: Can use not between
 delete m 
 --select AgeOfMedalist = m.OlympicYear - m.YearBorn
 from medalist m
@@ -20,11 +19,11 @@ from medalist m
 where (m.LastName like '% %' or m.LastName like '%-%')
 and m.LastName not like '% % %'
 
--- SM You're missing one question
 /*5 
 In invention delete all records where it is impossible that this inventor created this invention (based on year born or died). 
 In select statement show columns in this order Invention Name, Inventor Last Name, Year Invented, Year Born, Year Died, Age at time of invention
 */
+-- SM Tip: You can use not between
 delete i
 --select i.InventionName, i.InventorLastName, i.YearInvented, i.YearBorn, i.YearDied, AgeAtTimeOfInvention = i.YearInvented - i.YearBorn 
 from invention i 
