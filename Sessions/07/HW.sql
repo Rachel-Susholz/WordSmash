e2bb504a3727b05188df78ac049daf19c2e2d33c
Rachel-Susholz <rochelsusholz@gmail.com>
2023-12-04 15:14:41 -0500

    #6 - fixed homework

--- Sessions/07/HW.sql
+++ Sessions/07/HW.sql
@@ -8,15 +8,18 @@ When getting data from a source table always use the "source" data when you can,
       Janne's gold medal was entered into the database, but Jack's bronze medal was not. 
       Please add him in to correct this mistake. 
 */
-
+insert medalist(OlympicYear, Season, OlympicLocation, Sport, SportSubcategory, Medal, FirstName, LastName, Country, YearBorn)
+select 2002, 'winter', 'Athens, Greece', 'Skiing', 'Mens Moguls Freestyle', 'bronze', 'Jack', 'Lahtela', 'Austria', 1980
 --2. For the year 2008; create a new sport, award medals for your new sport to the same medalists that won the 2008 Women's Trampoline sport.
-
+insert medalist(OlympicYear, Season, OlympicLocation, Sport, SportSubcategory, Medal, FirstName, LastName, Country, YearBorn)
+select 2008, 'Winter', 'St. Moritz, Switzerland', 'Alpine Sliding', 'Womens', 'gold', 'Ekaterina', 'Khilko', 'Uzbekistan', 1982
+union select 2008, 'Winter', 'St. Moritz, Switzerland', 'Alpine Sliding', 'Womens', 'Silver', 'He', 'Wenna', 'China', 1989
+union select 2008, 'Winter', 'St. Moritz, Switzerland', 'Alpine Sliding', 'Womens', 'gold', 'Karen', 'Cockburn', 'Canada', 1980
 /*3. 
       The Men's 200 Metres Swimming records were mistakenly left out of the 2000 Summer Olympic Games. 
       Add in the gold, silver and bronze medalists for it. 
       The good news is that it's the same exact winners as the Field High Jump! 
 */
-
 
 /*4. 
      Add a new sport with new bronze, silver, and gold medalists for 2008. 
