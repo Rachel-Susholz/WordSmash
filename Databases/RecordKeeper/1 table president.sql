facf39baa0a88cb6d91eaa550069215a1947439e
Rachel-Susholz <rochelsusholz@gmail.com>
2024-08-05 09:36:42 -0400

    Part 1 Session #22 HW

--- Databases/RecordKeeper/1 table president.sql
+++ Databases/RecordKeeper/1 table president.sql
@@ -1,14 +1,16 @@
 -- paste this on the bottom of the table file
 use RecordKeeperDB
+go 
+drop table if exists party
+go
 drop table if exists president
 go
 		create table dbo.president(
-		PresidentId int not null identity (1000,1) primary key,
+		PresidentId int not null identity primary key,
 		Num int not null constraint u_President_num_must_be_unique unique
             constraint ck_President_num_must_be_greater_than_zero check(Num > 0), 
 		FirstName varchar(100) not null constraint ck_President_first_name_cannot_be_blank check(FirstName <> ''), 
         LastName varchar(100) not null constraint ck_President_last_name_cannot_be_blank check(LastName <> ''), 
-        Party varchar(50) not null constraint ck_President_party_cannot_be_blank check(Party <> ''),
         DateBorn date not null constraint ck_President_date_born_cannot_be_blank check(DateBorn <> '' ),  
 		DateDied datetime2,
 		TermStart int not null constraint ck_President_term_must_begin_after_1775 check(TermStart >= 1776),
@@ -28,4 +30,17 @@ go
 alter table President drop column if exists TermsServed
 go 
 alter table President add TermsServed as (TermEnd - Termstart) / 4  persisted
+go
+
+		create table dbo.party(
+		PartyId int not null identity primary key,
+		PresidentId int not null 
+			constraint f_president_party foreign key references President(PresidentId),
+		PartyName varchar (25) not null 
+			constraint c_party_name_cannot_be_blank check (PartyName <> ''),
+		YearFormed int not null,
+		Color varchar (10) not null 
+			constraint c_party_color_cannot_be_blank check (Color <> '')
+		)
+
 go
--- Databases/RocketDB/0 Database.sql
+++
@@ -1,7 +0,0 @@
-use master
-go
-drop database if exists RocketDB
-go 
-create database RocketDB 
-go 
-use RocketDB
