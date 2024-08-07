53ec0a0396e80700bc184118390ad8550886ffad
Rachel-Susholz <rochelsusholz@gmail.com>
2024-08-07 16:30:38 -0400

    Part 1 session # 22 HW #24

--- Databases/RecordKeeper/2 data president.sql
+++ Databases/RecordKeeper/2 data president.sql
@@ -1,64 +1,68 @@
 use RecordKeeperDB
-delete party 
-go
+
+delete executiveorders  
 delete president
-go 
-
-insert president(Num, FirstName, LastName, DateBorn, DateDied, TermStart, TermEnd)
-      select 1, 'George', 'Washington', '1732-02-22', '1799-12-14', 1789, 1797
-union select 2, 'John', 'Adams', '1735-10-30', '1826-07-04', 1797, 1801
-union select 3, 'Thomas', 'Jefferson', '1743-04-13', '1826-07-04', 1801, 1809
-union select 4, 'James', 'Madison', '1751-03-16', '1836-06-28', 1809, 1817
-union select 5, 'James', 'Monroe', '1758-04-28', '1831-07-04', 1817, 1825
-union select 6, 'John Quincy', 'Adams', '1767-07-11', '1848-02-23', 1825, 1829
-union select 7, 'Andrew', 'Jackson', '1767-03-15', '1845-06-08', 1829, 1837
-union select 8, 'Martin', 'van Buren', '1782-12-05', '1862-07-24', 1837, 1841
-union select 9, 'William H.', 'Harrison', '1773-02-09', '1841-04-04', 1841, 1841
-union select 10, 'John', 'Tyler', '1790-03-29', '1862-01-18', 1841, 1845
-union select 11, 'James K.', 'Polk', '1795-11-02', '1841-06-15', 1845, 1849
-union select 12, 'Zachary', 'Taylor', '1784-11-24', '1850-07-09', 1849, 1850
-union select 13, 'Millard', 'Fillmore', '1800-01-07', '1874-03-08', 1850, 1853
-union select 14, 'Franklin', 'Pierce', '1804-11-23', '1869-10-08', 1853, 1857
-union select 15, 'James', 'Buchanan', '1791-04-23', '1868-06-01', 1857, 1861
-union select 16, 'Abraham', 'Lincoln', '1809-02-12', '1865-04-15', 1861, 1865
-union select 17, 'Andrew', 'Johnson', '1808-12-29', '1875-07-31', 1865, 1869
-union select 18, 'Ulysses S.', 'Grant', '1822-04-27', '1885-07-23', 1869, 1877
-union select 19, 'Rutherford', 'Hayes', '1822-10-04', '1893-01-17', 1877, 1881
-union select 20, 'James', 'Garfield', '1831-11-19', '1881-09-19', 1881, 1881
-union select 21, 'Chester', 'Arthur', '1829-10-05', '1886-11-18', 1881, 1885
-union select 22, 'Grover', 'Cleveland', '1837-03-18', '1908-06-24', 1885, 1889
-union select 23, 'Benjamin', 'Harrison', '1833-08-20', '1901-03-13', 1889, 1893
-union select 24, 'Grover', 'Cleveland', '1837-03-18', '1908-06-24', 1893, 1897
-union select 25, 'William', 'McKinley', '1843-01-29', '1901-09-14', 1897, 1901
-union select 26, 'Theodore', 'Roosevelt', '1858-10-27', '1919-01-06', 1901, 1909
-union select 27, 'William', 'Taft', '1857-09-15', '1930-03-08', 1909, 1913
-union select 28, 'Woodrow', 'Wilson', '1856-12-28', '1924-02-03', 1913, 1921
-union select 29, 'Warren', 'Harding', '1865-11-02', '1923-08-02', 1921, 1923
-union select 30, 'Calvin', 'Coolidge', '1872-07-04', '1933-01-05', 1923, 1929
-union select 31, 'Herbert C.', 'Hoover', '1874-08-10', '1964-10-20', 1929, 1933
-union select 32, 'Franklin Delano', 'Roosevelt', '1882-01-30', '1945-04-12', 1933, 1945
-union select 33, 'Harry S', 'Truman', '1884-05-08', '1972-12-26', 1945, 1953
-union select 34, 'Dwight David', 'Eisenhower', '1890-10-14', '1969-03-28', 1953, 1961
-union select 35, 'John Fitzgerald', 'Kennedy', '1917-05-29', '1963-11-22', 1961, 1963
-union select 36, 'Lyndon Baines', 'Johnson', '1908-08-27', '1973-01-22', 1963, 1969
-union select 37, 'Richard Milhous', 'Nixon', '1913-01-09', '1994-04-22', 1969, 1974
-union select 38, 'Gerald R.', 'Ford', '1913-07-14', '2006-12-26', 1974, 1977
-union select 39, 'James Earl', 'Carter', '1924-10-01', null, 1977, 1981
-union select 40, 'Ronald Wilson', 'Reagan', '1911-02-06', '2004-06-05', 1981, 1989
-union select 41, 'George H. W.', 'Bush', '1924-06-12', '2018-11-30', 1989, 1993
-union select 42, 'William Jefferson', 'Clinton', '1946-08-19', null, 1993, 2001
-union select 43, 'George W.', 'Bush', '1946-07-06', null, 2001, 2009
-union select 44, 'Barack', 'Obama', '1961-08-04', null, 2009, 2017
-
-insert party(PresidentId, PartyName, YearFormed, Color)
-select PresidentId, 'Republican', 1854, 'Red' from president p where p.Num in (16, 18, 19, 20, 21, 23, 25, 26, 27, 29, 30, 31, 34, 37, 38, 40, 41, 43)
-union select p.PresidentId, 'Democrat', 1828, 'Blue' from president p where p.Num in (7, 8, 11, 14, 15, 22, 24, 28, 32, 33, 35, 36, 39, 42, 44)
-union select p.PresidentId, 'Federalist', 1791, 'Orange' from president p where p.Num = 2
-union select p.PresidentId, 'Whig', 1833, 'Yellow' from president p where p.Num in (9, 10, 12, 13)
-union select p.PresidentId, 'None,Federalist', 1789, 'White' from president p where p.Num = 1
-union select p.PresidentId, 'National Union', 1864, 'Green' from president p where p.Num = 17 
-union select p.PresidentId, 'Democratic-Republican', 1792, 'Purple' from president p where p.Num in (3, 4, 5, 6)
+delete party 
 
 
 
-select * from party
+insert party(PartyName, YearFormed, Color)
+select 'Republican', 1854, 'Red' 
+union select 'Democrat', 1828, 'Blue'
+union select 'Federalist', 1791, 'Orange' 
+union select 'Whig', 1833, 'Yellow' 
+union select 'None,Federalist', 1789, 'White' 
+union select 'National Union', 1864, 'Green' 
+union select 'Democratic-Republican', 1792, 'Purple'
+go
+
+insert president(PartyId, Num, FirstName, LastName, DateBorn, DateDied, TermStart, TermEnd)
+select r.PartyId, 1, 'George', 'Washington', '1732-02-22', '1799-12-14', 1789, 1797 from party r where r.PartyName = 'None,Federalist'
+union select r.PartyId, 2, 'John', 'Adams', '1735-10-30', '1826-07-04', 1797, 1801 from party r where r.PartyName = 'Federalist'
+union select r.PartyId, 3, 'Thomas', 'Jefferson', '1743-04-13', '1826-07-04', 1801, 1809 from party r where r.PartyName = 'Democratic-Republican'
+union select r.PartyId, 4, 'James', 'Madison', '1751-03-16', '1836-06-28', 1809, 1817 from party r where r.PartyName = 'Democratic-Republican'
+union select r.PartyId, 5, 'James', 'Monroe', '1758-04-28', '1831-07-04', 1817, 1825 from party r where r.PartyName = 'Democratic-Republican'
+union select r.PartyId, 6, 'John Quincy', 'Adams', '1767-07-11', '1848-02-23', 1825, 1829 from party r where r.PartyName = 'Democratic-Republican'
+union select r.PartyId, 7, 'Andrew', 'Jackson', '1767-03-15', '1845-06-08', 1829, 1837 from party r where r.PartyName = 'Democrat'
+union select r.PartyId, 8, 'Martin', 'van Buren', '1782-12-05', '1862-07-24', 1837, 1841 from party r where r.PartyName = 'Democrat'
+union select r.PartyId, 9, 'William H.', 'Harrison', '1773-02-09', '1841-04-04', 1841, 1841 from party r where r.PartyName = 'Whig'
+union select r.PartyId, 10, 'John', 'Tyler', '1790-03-29', '1862-01-18', 1841, 1845 from party r where r.PartyName = 'Whig'
+union select r.PartyId, 11, 'James K.', 'Polk', '1795-11-02', '1841-06-15', 1845, 1849 from party r where r.PartyName = 'Democrat'
+union select r.PartyId, 12, 'Zachary', 'Taylor', '1784-11-24', '1850-07-09', 1849, 1850 from party r where r.PartyName = 'Whig'
+union select r.PartyId, 13, 'Millard', 'Fillmore', '1800-01-07', '1874-03-08', 1850, 1853 from party r where r.PartyName = 'Whig'
+union select r.PartyId, 14, 'Franklin', 'Pierce', '1804-11-23', '1869-10-08', 1853, 1857 from party r where r.PartyName = 'Democrat'
+union select r.PartyId, 15, 'James', 'Buchanan', '1791-04-23', '1868-06-01', 1857, 1861 from party r where r.PartyName = 'Democrat'
+union select r.PartyId, 16, 'Abraham', 'Lincoln', '1809-02-12', '1865-04-15', 1861, 1865 from party r where r.PartyName = 'Republican'
+union select r.PartyId, 17, 'Andrew', 'Johnson', '1808-12-29', '1875-07-31', 1865, 1869 from party r where r.PartyName = 'National Union'
+union select r.PartyId, 18, 'Ulysses S.', 'Grant', '1822-04-27', '1885-07-23', 1869, 1877 from party r where r.PartyName = 'Republican'
+union select r.PartyId, 19, 'Rutherford', 'Hayes', '1822-10-04', '1893-01-17', 1877, 1881 from party r where r.PartyName = 'Republican'
+union select r.PartyId, 20, 'James', 'Garfield', '1831-11-19', '1881-09-19', 1881, 1881 from party r where r.PartyName = 'Republican'
+union select r.PartyId, 21, 'Chester', 'Arthur', '1829-10-05', '1886-11-18', 1881, 1885 from party r where r.PartyName = 'Republican'
+union select r.PartyId, 22, 'Grover', 'Cleveland', '1837-03-18', '1908-06-24', 1885, 1889 from party r where r.PartyName = 'Democrat'
+union select r.PartyId, 23, 'Benjamin', 'Harrison', '1833-08-20', '1901-03-13', 1889, 1893 from party r where r.PartyName = 'Republican'
+union select r.PartyId, 24, 'Grover', 'Cleveland', '1837-03-18', '1908-06-24', 1893, 1897 from party r where r.PartyName = 'Democrat'
+union select r.PartyId, 25, 'William', 'McKinley', '1843-01-29', '1901-09-14', 1897, 1901 from party r where r.PartyName = 'Republican'
+union select r.PartyId, 26, 'Theodore', 'Roosevelt', '1858-10-27', '1919-01-06', 1901, 1909 from party r where r.PartyName = 'Republican'
+union select r.PartyId, 27, 'William', 'Taft', '1857-09-15', '1930-03-08', 1909, 1913 from party r where r.PartyName = 'Republican'
+union select r.PartyId, 28, 'Woodrow', 'Wilson', '1856-12-28', '1924-02-03', 1913, 1921 from party r where r.PartyName = 'Democrat'
+union select r.PartyId, 29, 'Warren', 'Harding', '1865-11-02', '1923-08-02', 1921, 1923 from party r where r.PartyName = 'Republican'
+union select r.PartyId, 30, 'Calvin', 'Coolidge', '1872-07-04', '1933-01-05', 1923, 1929 from party r where r.PartyName = 'Republican'
+union select r.PartyId, 31, 'Herbert C.', 'Hoover', '1874-08-10', '1964-10-20', 1929, 1933 from party r where r.PartyName = 'Republican'
+union select r.PartyId, 32, 'Franklin Delano', 'Roosevelt', '1882-01-30', '1945-04-12', 1933, 1945 from party r where r.PartyName = 'Democrat'
+union select r.PartyId, 33, 'Harry S', 'Truman', '1884-05-08', '1972-12-26', 1945, 1953 from party r where r.PartyName = 'Democrat'
+union select r.PartyId, 34, 'Dwight David', 'Eisenhower', '1890-10-14', '1969-03-28', 1953, 1961 from party r where r.PartyName = 'Republican'
+union select r.PartyId, 35, 'John Fitzgerald', 'Kennedy', '1917-05-29', '1963-11-22', 1961, 1963 from party r where r.PartyName = 'Democrat'
+union select r.PartyId, 36, 'Lyndon Baines', 'Johnson', '1908-08-27', '1973-01-22', 1963, 1969 from party r where r.PartyName = 'Democrat'
+union select r.PartyId, 37, 'Richard Milhous', 'Nixon', '1913-01-09', '1994-04-22', 1969, 1974 from party r where r.PartyName = 'Republican'
+union select r.PartyId, 38, 'Gerald R.', 'Ford', '1913-07-14', '2006-12-26', 1974, 1977 from party r where r.PartyName = 'Republican'
+union select r.PartyId, 39, 'James Earl', 'Carter', '1924-10-01', null, 1977, 1981 from party r where r.PartyName = 'Democrat'
+union select r.PartyId, 40, 'Ronald Wilson', 'Reagan', '1911-02-06', '2004-06-05', 1981, 1989 from party r where r.PartyName = 'Republican'
+union select r.PartyId, 41, 'George H. W.', 'Bush', '1924-06-12', '2018-11-30', 1989, 1993 from party r where r.PartyName = 'Republican'
+union select r.PartyId, 42, 'William Jefferson', 'Clinton', '1946-08-19', null, 1993, 2001 from party r where r.PartyName = 'Democrat'
+union select r.PartyId, 43, 'George W.', 'Bush', '1946-07-06', null, 2001, 2009 from party r where r.PartyName = 'Republican'
+union select r.PartyId, 44, 'Barack', 'Obama', '1961-08-04', null, 2009, 2017 from party r where r.PartyName = 'Democrat'
+go
+
+
+
+
--- Databases/RecordKeeper/3 join-queries president.sql
+++ Databases/RecordKeeper/3 join-queries president.sql
@@ -6,41 +6,48 @@ order by e.PageNumber
 --b) same as (a) but include the presidents name
 select ExecutiveOrders = concat(p.FirstName, ', ', p.LastName, ': ', 'Order No. ', e.OrderNumber, ', ', e.VolumeNumber, ' ', e.OrderCode, ' ', e.PageNumber, ' ', e.IssueYear, '. ', e.OrderName)
 from president p
-join party r 
-on p.PresidentId = r.PresidentId
 join ExecutiveOrders e 
-on r.PartyId = e.PartyId
+on p.PresidentId = e.PresidentId
 order by e.PageNumber
 --c) same as (b) but include the party name
 select ExecutiveOrders = concat(p.FirstName, ', ', p.LastName, ' - ', r.PartyName, ': ', 'Order No. ', e.OrderNumber, ', ', e.VolumeNumber, ' ', e.OrderCode, ' ', e.PageNumber, ' ', e.IssueYear, '. ', e.OrderName)
-from president p
-join party r 
-on p.PresidentId = r.PresidentId
+from party r
+join president p 
+on r.PartyId = p.PartyId
 join ExecutiveOrders e 
-on r.PartyId = e.PartyId
+on p.PresidentId = e.PresidentId
 order by e.PageNumber
 --d) show number of executive orders per president for presidents that issued 3 or more orders. sort by highest number of orders
 select NumOfOrders = count(*), p.LastName
 from president p
-join party r 
-on p.PresidentId = r.PresidentId
 join ExecutiveOrders e 
-on r.PartyId = e.PartyId
+on p.PresidentId = e.PresidentId
 group by  p.LastName
 having count(*) > = 3 
 order by NumOfOrders desc 
 --e) show number of executive orders per party
 select NumOfOrders = count(*), r.PartyName
 from party r
+join President p
+on r.PartyId = p.PartyId
 join ExecutiveOrders e 
-on r.PartyId = e.PartyId
+on p.PresidentId = e.PresidentId
 group by r.PartyName 
 --f) pick a party that has one or more executive orders and delete the party
 delete e
 --select *
 from party r
+join president p 
+on r.PartyId = p.PartyId
 join ExecutiveOrders e 
-on r.PartyId = e.PartyId
+on p.PresidentId = e.PresidentId
+where r.PartyName = 'National Union'
+
+delete p
+--select *
+from party r
+join president p 
+on r.PartyId = p.PartyId
 where r.PartyName = 'National Union'
 
 delete r 
@@ -52,6 +59,8 @@ update e
 set e.OrderStatus = 0
 --select e.OrderStatus 
 from party r
+join president p 
+on r.PartyId = p.PartyId
 join ExecutiveOrders e 
-on r.PartyId = e.PartyId
+on p.PresidentId = e.PresidentId
 where r.PartyName = 'Federalist'
