8c9cc5b7d8aad82821746c6eff11535af7ece358
Rachel-Susholz <rochelsusholz@gmail.com>
2024-08-13 05:31:40 -0400

    Part 1 Session 25B HW #30

--- Sessions/25b/HW.sql
+++ Sessions/25b/HW.sql
@@ -1,15 +1,10 @@
 --AS Great job! 98% See comment and resubmit.
 --1 All presidents are participating in the Olympics! In all sports! Show me a list of presidents (Number, Last Name), and each sport in the medalist table. Sort by president number.
 --AS -2 No need for a CTE here. You can do the same thing with a basic cross join
-;
-with x as 
-    (
-    select distinct m.Sport
-    from medalist m
-    )
-select President = concat (p.Num,  ', ', p.LastName), x.Sport
-from x
-cross join president p 
+
+select distinct m.sport, President = concat (p.Num,  ', ', p.LastName) 
+from president p 
+cross join medalist m 
 order by p.Num
 /*
 --2 None of the presidents won any olympic medals. 
