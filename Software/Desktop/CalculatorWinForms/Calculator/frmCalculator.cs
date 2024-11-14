c2e19d9431cc533d0dc0c98eb12d89b6e95ce78e
Rachel-Susholz <rochelsusholz@gmail.com>
2024-11-14 11:26:05 -0500

    Part 2 Session 5 HW #36

--- Software/Desktop/CalculatorWinForms/Calculator/frmCalculator.cs
+++ Software/Desktop/CalculatorWinForms/Calculator/frmCalculator.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Reflection.Metadata.Ecma335;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -12,6 +13,7 @@ namespace Calculator
 {
     public partial class frmCalculator : Form
     {
+       
         public frmCalculator()
         {
             InitializeComponent();
@@ -30,7 +32,72 @@ namespace Calculator
             btnMultiply.Click += BtnMultiply_Click;
             btnSubtract.Click += BtnSubtract_Click;
             btnClear.Click += BtnClear_Click;
+            btnDecimal.Click += BtnDecimal_Click;
+            btnSign.Click += BtnSign_Click;
+            btnEquals.Click += BtnEquals_Click;
         }
+
+        private int DetermineActiveBox()
+
+        {
+            int n = 0;
+            // Return 1 if the first factor should be active
+            if (txtFactor1.Text == "" || txtOperator.Text == "")
+            {
+                n = 1;
+            }
+            // Return 2 if the second factor should be active and the answer is empty
+            else if (txtFactor1.Text != "" && txtOperator.Text != "" && txtAnswer.Text == "")
+            {
+                return 2;
+            }
+            // Return 0 if none are active (answer already filled)
+            return n;
+        }
+
+        private string GetActiveBoxFactorValue()
+        {
+            int n = DetermineActiveBox();
+            string s = "";
+
+            if (n == 1)
+            {
+                s = txtFactor1.Text;
+            }
+            else if (n == 2)
+            {
+                s = txtFactor2.Text;
+            }
+
+            return s;
+        }
+
+
+        private void InputToActiveBox(string value)
+        {
+            int n = DetermineActiveBox();
+
+            if (n == 1)
+            {
+                // Append value to txtFactor1 if it's active
+                txtFactor1.Text += value;
+            }
+            else if (n == 2)
+            {
+                // Append value to txtFactor2 if it's active
+                txtFactor2.Text += value;
+            }
+        }
+
+        private void ClearInvalidInputs()
+        {
+            txtFactor1.Text = "";
+            txtFactor2.Text = "";
+            txtOperator.Text = "";
+            txtAnswer.Text = "";
+        }
+
+
 
         private void BtnClear_Click(object? sender, EventArgs e)
         {
@@ -42,78 +109,178 @@ namespace Calculator
         }
 
         private void BtnSubtract_Click(object? sender, EventArgs e)
-        {
-            txtOperator.Text = "-";
+        { 
+            if (txtFactor1.Text != "")
+            {
+                txtOperator.Text = "-";
+            }
         }
-
         private void BtnMultiply_Click(object? sender, EventArgs e)
         {
-            txtOperator.Text = "x";
+            if (txtFactor1.Text != "")
+            {
+                txtOperator.Text = "x";
+            }
         }
 
         private void BtnDivide_Click(object? sender, EventArgs e)
         {
-            txtOperator.Text = "/";
+            if (txtFactor1.Text != "")
+            {
+                txtOperator.Text = "/";
+            }
         }
 
         private void BtnAdd_Click(object? sender, EventArgs e)
         {
-            txtOperator.Text = "+";
+            if (txtFactor1.Text != "")
+            {
+                txtOperator.Text = "+";
+            }
         }
 
         private void Btn9_Click(object? sender, EventArgs e)
         {
-            txtFactor1.Text = txtFactor1.Text + 9;
+            InputToActiveBox("9");
         }
 
         private void Btn8_Click(object? sender, EventArgs e)
         {
-            txtFactor1.Text = txtFactor1.Text + 8;
+            InputToActiveBox("8");
         }
 
         private void Btn7_Click(object? sender, EventArgs e)
         {
-            txtFactor1.Text = txtFactor1.Text + 7;
+            InputToActiveBox("7");
         }
 
         private void Btn6_Click(object? sender, EventArgs e)
         {
-            txtFactor1.Text = txtFactor1.Text + 6;
+            InputToActiveBox("6");
         }
 
         private void Btn5_Click(object? sender, EventArgs e)
         {
-            txtFactor1.Text = txtFactor1.Text + 5;
+            InputToActiveBox("5");
         }
 
         private void Btn4_Click(object? sender, EventArgs e)
         {
-            txtFactor1.Text = txtFactor1.Text + 4;
+            InputToActiveBox("4");
         }
 
         private void Btn3_Click(object? sender, EventArgs e)
         {
-            txtFactor1.Text = txtFactor1.Text + 3;
+            InputToActiveBox("3");
         }
 
         private void Btn2_Click(object? sender, EventArgs e)
         {
-            txtFactor1.Text = txtFactor1.Text + 2;
+            InputToActiveBox("2");
         }
 
         private void Btn1_Click(object? sender, EventArgs e)
         {
-            txtFactor1.Text = txtFactor1.Text + 1;
+            InputToActiveBox("1");
         }
 
         private void Btn0_Click(object? sender, EventArgs e)
         {
-            txtFactor1.Text = txtFactor1.Text + 0;
+            InputToActiveBox("0");
         }
 
         private void tblMain_Paint(object sender, PaintEventArgs e)
         {
 
         }
+        // This just popped up, not sure how to get rid of it withought going to the back file.
+        private void BtnDecimal_Click(object? sender, EventArgs e)
+        {
+            if (!GetActiveBoxFactorValue().Contains("."))
+            {
+                InputToActiveBox(".");
+            }
+        }
+        private void BtnSign_Click(object? sender, EventArgs e)
+        {
+            //
+            if (GetActiveBoxFactorValue().StartsWith("-"))
+            {
+                int n = DetermineActiveBox();
+                string s = GetActiveBoxFactorValue().Substring(1);
+                if (n == 1)
+                {
+                    txtFactor1.Text = s;
+                }
+                else if (n == 2)
+                {
+                    txtFactor2.Text = s;
+                }
+            }
+            else if (GetActiveBoxFactorValue() == "")
+            {
+                InputToActiveBox("-");
+            }
+            else
+            {
+                int n = DetermineActiveBox();
+                string s = ("-") + GetActiveBoxFactorValue();
+                if (n == 1)
+                {
+                    txtFactor1.Text = s;
+                }
+                else if (n == 2)
+                {
+                    txtFactor2.Text = s;
+                }
+            }
+        }
+        private void BtnEquals_Click(object? sender, EventArgs e)
+        {
+            decimal factor1, factor2;
+            string s = "";
+            // Check for empty input fields
+            if (txtFactor1.Text == s || txtFactor2.Text == s || txtOperator.Text == s)
+            {
+                return;
+            }
+
+            // Try parsing factor1 and factor2 as decimals
+            if (!decimal.TryParse(txtFactor1.Text, out factor1) || !decimal.TryParse(txtFactor2.Text, out factor2))
+            {
+                // Clear invalid inputs if parsing fails
+                ClearInvalidInputs();
+                return;
+            }
+
+            
+            if (txtOperator.Text == "+")
+            {
+                txtAnswer.Text = (factor1 + factor2).ToString();
+            }
+            else if (txtOperator.Text == "-")
+            {
+                txtAnswer.Text = (factor1 - factor2).ToString();
+            }
+            else if (txtOperator.Text == "x")
+            {
+                txtAnswer.Text = (factor1 * factor2).ToString();
+            }
+            else if (txtOperator.Text == "/")
+            {
+                if (factor2 == 0)
+                {
+                    ClearInvalidInputs();
+                    return;
+                }
+                txtAnswer.Text = (factor1 / factor2).ToString();
+            }
+            else
+            {
+                ClearInvalidInputs();
+            }
+            txtAnswer.ReadOnly = true;
+        }
     }
 }
+
